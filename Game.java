fec4d04aacae5de0bf9d60d1d4cf4461128f84be
Paul Cummings <paulcummings01@gmail.com>
2018-09-10 20:10:55 -0700

    Update Game.java

--- Game.java
+++ Game.java
@@ -1,17 +1,86 @@
 package com;
 
-public class Game {
-	Board gameBoard;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.EventQueue;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Label;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.geom.Rectangle2D;
+
+import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JPanel;
+
+public class Game extends JPanel {
+    final int WINDOW_WIDTH = 1000;
+    final int WINDOW_HEIGHT = 800;
+    
+    final int SQUARE_SIZE = 68;
+    final int X_START = 56;
+    final int Y_START = 26;
+    
+    Board gameBoard;
+	int ySquares;
+	int xSquares;
+
 	boolean gameStarted;
 	boolean gameWon;
 	
+	JFrame frame;
+	Graphics g;
+	
+	
 	public Game() {
 		gameBoard = new Board();
-		gameStarted=false;
-		gameWon=false;
+		ySquares = gameBoard.getYSquares();
+		xSquares = gameBoard.getXSquares();
+		gameStarted = true;
+		gameWon = false;
 		
-		newGame();
 	}
+	
+	public void paintComponent (Graphics g) {
+		super.paintComponent( g );
+
+		Graphics2D g2d = (Graphics2D ) g;
+
+		int currentY = Y_START;
+		int currentX = X_START;
+		for(int y = 0; y < ySquares; y++) {
+			for( int x = 0; x < xSquares; x++) {
+				if(y%2 == 0) {
+					if(x%2 == 0) {
+						g2d.setPaint( Color.WHITE);
+						g2d.fill( new Rectangle2D.Double( currentX, currentY, SQUARE_SIZE, SQUARE_SIZE));
+						currentX += SQUARE_SIZE;
+					} else {
+						g2d.setPaint( Color.BLACK);
+						g2d.fill( new Rectangle2D.Double( currentX, currentY, SQUARE_SIZE, SQUARE_SIZE));
+						currentX += SQUARE_SIZE;						
+					}
+				} else {
+					if(x%2 == 0) {
+						g2d.setPaint( Color.BLACK);
+						g2d.fill( new Rectangle2D.Double( currentX, currentY, SQUARE_SIZE, SQUARE_SIZE));
+						currentX += SQUARE_SIZE;						
+					} else {
+						g2d.setPaint( Color.WHITE);
+						g2d.fill( new Rectangle2D.Double( currentX, currentY, SQUARE_SIZE, SQUARE_SIZE));
+						currentX += SQUARE_SIZE;						
+					}					
+				}
+            }
+			currentY += SQUARE_SIZE;
+			currentX = X_START;
+        }
+	}
+	
+
 	
 	private void newGame() {
 		gameBoard = new Board();
@@ -21,11 +90,12 @@ public class Game {
 	}
 	
 	private void drawBoard() {
-		int y = gameBoard.getYSquares();
-		int x = gameBoard.getXSquares();
+
 		
-		for (int i=0; i<y; i++) {
-			for (int j=0; j<x; j++) {
+		g = frame.getGraphics();
+		
+		for (int i=0; i<ySquares; i++) {
+			for (int j=0; j<xSquares; j++) {
 				
 				if (gameBoard.isEmpty(i,j) ) {
 					
@@ -43,8 +113,15 @@ public class Game {
 			System.out.print("\n");
 		}
 	}
-	public static void main(String[] args) {
-		System.out.println("Welcome");
-		Game myGame = new Game();
-	}
+	public static void main(String args[]) {
+	    JFrame frame = new JFrame("Checkers");
+	    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+	    Game panel = new Game();
+	    
+	    frame.add(panel);
+
+	    frame.setSize(1000, 800);
+	    frame.setVisible(true);
+    }
 }
